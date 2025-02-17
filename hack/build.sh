76f44a2343c0207f071079658bef3021a1df9591
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-02-17 13:43:12 +0100

    Build works
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/build.sh
+++ hack/build.sh
@@ -2,9 +2,22 @@
 
 source "$(dirname $0)/common.sh"
 
+err_report() {
+    echo "!!! Error on line $1 !!!"
+    exit 1
+}
+
+set -T          # inherit DEBUG and RETURN trap for functions
+set -C          # prevent file overwrite by > &> <>
+set -E          # inherit -e
+set -e          # exit immediately on errors
+set -u          # exit on not assigned variables
+set -o pipefail # exit on pipe failure
+
+trap 'err_report $LINENO' ERR
+
 export KO_DOCKER_REPO="${KO_DOCKER_REPO:-kind.local}"
 
 build_transform_jsonata_image || exit $?
-push_transform_jsonata_image || exit $?
 
-#build_integration_images || exit $?
+build_integration_images || exit $?
--- hack/common.sh
+++ hack/common.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 export TAG=${TAG:-$(git rev-parse HEAD)}
 
+export KO_DOCKER_REPO="${KO_DOCKER_REPO:-kind.local}"
 export TRANSFORM_JSONATA_IMAGE_WITH_TAG="${KO_DOCKER_REPO}/transform-jsonata:${TAG}"
 
 [[ ! -v REPO_ROOT_DIR ]] && REPO_ROOT_DIR="$(git rev-parse --show-toplevel)"
@@ -12,19 +13,18 @@ readonly REPO_ROOT_DIR
 function build_transform_jsonata_image() {
 
   docker version
+  docker buildx version
 
-  docker buildx build \
+  docker buildx build --debug \
     --platform "linux/amd64" \
     -t "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}-amd64" \
     -f "${REPO_ROOT_DIR}/transform-jsonata/Dockerfile" \
-    --output=type=docker \
     "${REPO_ROOT_DIR}/transform-jsonata" || return $?
 
-  docker buildx build \
+  docker buildx build --debug \
     --platform "linux/arm64" \
     -t "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}-arm64" \
     -f "${REPO_ROOT_DIR}/transform-jsonata/Dockerfile" \
-    --output=type=docker \
     "${REPO_ROOT_DIR}/transform-jsonata" || return $?
 }
 
@@ -33,15 +33,18 @@ function push_transform_jsonata_image() {
     docker push "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}-arm64" || return $?
 
     echo "Creating manifest ${TRANSFORM_JSONATA_IMAGE_WITH_TAG}"
-    docker manifest create "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}" \
+    docker manifest create --amend "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}" \
         "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}-amd64" \
         "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}-arm64" || return $?
 
     echo "Pushing manifest ${TRANSFORM_JSONATA_IMAGE_WITH_TAG}"
     docker manifest push "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}" || return $?
-    # TODO figure out digest properly
-#    TRANSFORM_JSONATA_IMAGE=$(docker inspect --format '{{index .RepoDigests 0}}' "${image}")
-#    export TRANSFORM_JSONATA_IMAGE
+
+    digest=$(docker buildx imagetools inspect "${TRANSFORM_JSONATA_IMAGE_WITH_TAG}" --format "{{json .Manifest.Digest }}" | tr -d '"')
+    TRANSFORM_JSONATA_IMAGE="${TRANSFORM_JSONATA_IMAGE_WITH_TAG}@${digest}"
+    export TRANSFORM_JSONATA_IMAGE
+
+    echo "TRANSFORM_JSONATA_IMAGE=${TRANSFORM_JSONATA_IMAGE}"
 }
 
 function build_integration_images() {
